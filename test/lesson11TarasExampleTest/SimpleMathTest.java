360a29d201dda05881b3afc57a57963b3282db32
BliznyukS <bliznyuk.sergey@gmail.com>
2016-02-11 22:38:06 +0200

    DZ Modul 11.1

--- test/lesson11TarasExampleTest/SimpleMathTest.java
+++ test/lesson11TarasExampleTest/SimpleMathTest.java
@@ -1,11 +1,20 @@
 package lesson11TarasExampleTest;
 
+import lesson11TarasExample.SimpleMath;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SimpleMathTest {
 
     @org.junit.Test
     public void testAdd() throws Exception {
+        int a = 4;
+        int b = 7;
 
+        final SimpleMath simpleMath = new SimpleMath();
+
+        final int result = simpleMath.add(a, b);
+
+        Assert.assertEquals(11, result);
     }
 }
